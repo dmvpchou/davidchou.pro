fb8e17d69f0b756f427ccfc8f300b6d03200dbd8
daweiichou <im@davidchou.pro>
2024-07-10 22:53:30 +0800

    add for git action

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astroship-pro.web3templates.com",
+  site: "https://davidchou.pro",
   image: {
     domains: ["unsplash.com", "images.unsplash.com"],
   },
